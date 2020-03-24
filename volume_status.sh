e54c65677ae2e67c613fc33e3126706f44bab8be
Eric Lay <lay.eric@gmail.com>
2020-03-24 14:12:06 -0500

    added limepanel/volume_status mods

---
+++ volume_status.sh
@@ -0,0 +1,34 @@
+#!/bin/dash
+. ~/.limepanelrc
+
+panel_volicon()
+{
+        volStatus=$(pulseaudio-ctl full-status | awk '{print $2}')
+        volLevel=$(pulseaudio-ctl C)
+
+        if [ "$volStatus" = "yes" ]
+                then echo "$muted_icon"
+        elif [ "$volStatus" = "no" ]
+                then echo "$unmuted_icon "
+		else echo " ?"
+        fi
+}
+
+panel_avolicon()
+{
+        avolStatus=$(avol state)
+        avolLevel=$(avol level)
+
+        if [ "$avolStatus" = "off" ]
+                then echo "$muted_icon"
+        elif [ "$avolStatus" = "on" ]
+                then echo "$unmuted_icon"
+		else echo " ?"
+        fi
+}
+
+if [ $(pgrep pulseaudio >/dev/null) ]; then
+		echo "V"%{A:dvol:}%{A2:volume mute:}%{A5:volume down 1:}%{A4:volume up 1:}$(panel_volicon)%{A}%{A}%{A}%{A}> "/tmp/panel-fifo1" 
+	else
+		echo "V"%{A:dvol:}%{A2:volume mute:}%{A5:volume down 1:}%{A4:volume up 1:}$(panel_avolicon)%{A}%{A}%{A}%{A}> "/tmp/panel-fifo1"
+fi
